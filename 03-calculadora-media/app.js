33b52a89a4d116517e2e88ec0d16b053dad291a7
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-03-16 11:30:34 -0300

    finalizado por hoje

--- 03-calculadora-media/app.js
+++ 03-calculadora-media/app.js
@@ -1,2 +1,76 @@
 "use strict"
+//declarando variávreis correspondentes ao html
+const inserirNota1 = document.getElementById("inserir-nota1")
+const inserirNota2 = document.getElementById("inserir-nota2")
+const inserirNota3 = document.getElementById("inserir-nota3")
+const exibirResultado = document.getElementById("exibir-resultado")
 
+
+function mostrarValores(){
+console.log("teste")
+}
+//função para mostrar os valores
+const mostrarValores2 = function(){
+
+console.log("teste")
+
+    // let mediaAluno
+    // mediaAluno = calcularMedia(inserirNota1.value, inserirNota2.value, inserirNota3.value)
+
+    // let situacaoAluno
+    // situacaoAluno = definirSituacao(mediaAluno)
+
+    // if(situacaoAluno == "APROVADO"){
+    //     exibirResultado.classList.remove("aprovado", "reprovado", "recuperacao")
+    //     exibirResultado.classList.add("aprovado")
+    //     return exibirResultado.textContent = (`Média do Aluno(a):${mediaAluno}\nSituação do Aluno(a):${situaçãoAluno}`)
+
+    // }else if(situacaoAluno == "REPROVADO"){
+    //     exibirResultado.classList.remove("aprovado", "reprovado", "recuperacao")
+    //     exibirResultado.classList.add("reprovado")
+    //     return exibirResultado.textContent = (`Média do Aluno(a):${mediaAluno}\nSituação do Aluno(a):${situaçãoAluno}`)
+        
+    // }else if(situacaoAluno == "RECUPERAÇÃO"){
+    //     exibirResultado.classList.remove("aprovado", "reprovado", "recuperacao")
+    //     exibirResultado.classList.add("recuperacao")
+    //     return exibirResultado.textContent = (`Média do Aluno(a):${mediaAluno}\nSituação do Aluno(a):${situaçãoAluno}`)
+        
+    // }else{
+    //     return false
+    // }
+
+}
+
+//função para calcular a média
+const calcularMedia = function(valor1,valor2,valor3){
+    //criando váriavéis necessárias
+    let nota1 = Number(valor1)
+    let nota2 = Number(valor2)
+    let nota3 = Number(valor3)
+    let mediaFinal
+
+    //realizando o calculo da média
+    mediaFinal = (nota1 + nota2 + nota3) / 3
+
+    return mediaFinal
+}
+
+//função para definir a situação do aluno
+const definirSituacao = function(media){
+    //criando váriavéis necessárias
+    let mediaAluno = Number(media)
+    let situaçãoAluno
+
+    //condicionais para definir situação
+    if(mediaAluno >= 7)
+        return situaçãoAluno = "APROVADO"
+    else if(mediaAluno <5)
+        return situaçãoAluno = "REPROVADO"
+    else if(mediaAluno >= 5 || mediaAluno < 7)
+        return situaçãoAluno = "RECUPERAÇÃO"
+    else{
+        return false
+    }
+}
+
+.addEventListener("click", mostrarValores)
